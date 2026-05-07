1547df600ebe0c895fbab9921709daa37c7f1692
Luiz Renato <ruizlenato@proton.me>
2026-05-07 00:07:34 -0300

    build: bump to version 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.ruizlenato.karabau"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
